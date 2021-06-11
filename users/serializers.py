d4b63e49ea85508fbf776c95e7522e070ed300b7
Tom <tom4ke@gmail.com>
2021-06-11 23:47:43 +0600

    local changes

--- users/serializers.py
+++ users/serializers.py
@@ -7,7 +7,7 @@ from django.contrib.auth import authenticate
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email')
+        fields = ('id', 'username', 'email', 'phone_number', 'avatar')
 
 
 # Register Serializer
